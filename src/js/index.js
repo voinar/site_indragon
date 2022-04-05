5e0c06f20b0c47d3e15e26ceaa068192c46954c4
marcin <38013623+voinar@users.noreply.github.com>
2022-04-05 22:08:21 +0200

    new protfolio slider added
    infinite slider

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@
 // - preloader mask fade in
 // - hero section typewriter effect
 // - navbar behavior
-// - portfolio section scroll
+// - portfolio section slider
 //------------------------
 
 
@@ -184,92 +184,213 @@ new Typewriter('#typewriter', {
 
 
 //------------------------
-// PORTFOLIO NAVIGATION SCROLL
+// PORTFOLIO NAVIGATION SLIDER
 //------------------------
 
-const navigationLeft = document.querySelector('.navigation-left');
-const navigationRight = document.querySelector('.navigation-right');
-const navigationArrows = document.querySelector('.navigation-arrows');
-const portfolioProjectCards = document.querySelector('.portfolio-project-cards');
-var currentMarginOffset = parseInt(window.getComputedStyle(portfolioProjectCards).getPropertyValue('margin-left'));
+var slider = document.getElementById('slider'),
+    sliderItems = document.getElementById('slides'),
+    prev = document.getElementById('prev'),
+    next = document.getElementById('next');
 
+function slide(wrapper, items, prev, next) {
+  var posX1 = 0,
+      posX2 = 0,
+      posInitial,
+      posFinal,
+      threshold = 100,
+      slides = items.getElementsByClassName('slide'),
+      slidesLength = slides.length,
+      slideSize = items.getElementsByClassName('slide')[0].offsetWidth,
+      firstSlide = slides[0],
+      lastSlide = slides[slidesLength - 1],
+      cloneFirst = firstSlide.cloneNode(true),
+      cloneLast = lastSlide.cloneNode(true),
+      index = 0,
+      allowShift = true;
+  
+  // Clone first and last slide
+  items.appendChild(cloneFirst);
+  items.insertBefore(cloneLast, firstSlide);
+  wrapper.classList.add('loaded');
+  
+  // Mouse events
+  items.onmousedown = dragStart;
+  
+  // Touch events
+  items.addEventListener('touchstart', dragStart);
+  items.addEventListener('touchend', dragEnd);
+  items.addEventListener('touchmove', dragAction);
+  
+  // Click events
+  prev.addEventListener('click', function () { shiftSlide(-1) });
+  next.addEventListener('click', function () { shiftSlide(1) });
+  
+  // Transition events
+  items.addEventListener('transitionend', checkIndex);
+  
+  function dragStart (e) {
+    e = e || window.event;
+    e.preventDefault();
+    posInitial = items.offsetLeft;
+    
+    if (e.type == 'touchstart') {
+      posX1 = e.touches[0].clientX;
+    } else {
+      posX1 = e.clientX;
+      document.onmouseup = dragEnd;
+      document.onmousemove = dragAction;
+    }
+  }
 
-var portfolioCard = document.querySelector('.portfolioCard');
-var newMarginOffset = 0;
+  function dragAction (e) {
+    e = e || window.event;
+    
+    if (e.type == 'touchmove') {
+      posX2 = posX1 - e.touches[0].clientX;
+      posX1 = e.touches[0].clientX;
+    } else {
+      posX2 = posX1 - e.clientX;
+      posX1 = e.clientX;
+    }
+    items.style.left = (items.offsetLeft - posX2) + "px";
+  }
+  
+  function dragEnd (e) {
+    posFinal = items.offsetLeft;
+    if (posFinal - posInitial < -threshold) {
+      shiftSlide(1, 'drag');
+    } else if (posFinal - posInitial > threshold) {
+      shiftSlide(-1, 'drag');
+    } else {
+      items.style.left = (posInitial) + "px";
+    }
 
-function getCardWidth() {
-    return portfolioCard.width + 20;
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+  
+  function shiftSlide(dir, action) {
+    items.classList.add('shifting');
+    
+    if (allowShift) {
+      if (!action) { posInitial = items.offsetLeft; }
+
+      if (dir == 1) {
+        items.style.left = (posInitial - slideSize) + "px";
+        index++;      
+      } else if (dir == -1) {
+        items.style.left = (posInitial + slideSize) + "px";
+        index--;      
+      }
+    };
+    
+    allowShift = false;
+  }
+    
+  function checkIndex (){
+    items.classList.remove('shifting');
+
+    if (index == -1) {
+      items.style.left = -(slidesLength * slideSize) + "px";
+      index = slidesLength - 1;
+    }
+
+    if (index == slidesLength) {
+      items.style.left = -(1 * slideSize) + "px";
+      index = 0;
+    }
+    
+    allowShift = true;
+  }
 }
+
+slide(slider, sliderItems, prev, next);
+
+
+
+// const navigationLeft = document.querySelector('.navigation-left');
+// const navigationRight = document.querySelector('.navigation-right');
+// const navigationArrows = document.querySelector('.navigation-arrows');
+// const portfolioProjectCards = document.querySelector('.portfolio-project-cards');
+// var currentMarginOffset = parseInt(window.getComputedStyle(portfolioProjectCards).getPropertyValue('margin-left'));
+
+
+// var portfolioCard = document.querySelector('.portfolioCard');
+// var newMarginOffset = 0;
+
+// function getCardWidth() {
+//     return portfolioCard.width + 20;
+// }
+
+// // function scrollLeft() {
+// //     var intViewportWidth = window.innerWidth;
+// //     navigationRight.style.opacity = "1";
+// //     navigationLeft.style.opacity = "1";
+
+// //     if (intViewportWidth >= 1000) { //scroll behavior for viewport width over 1000px
+// //         if (currentMarginOffset < 0) {
+// //             currentMarginOffset += scrollOffset;
+// //             portfolioProjects.style.marginLeft = currentMarginOffset + "rem";
+// //         } else {
+// //             navigationLeft.style.opacity = ".3";
+// //         }
+// //     } else if (intViewportWidth < 1000) { //scroll behavior for viewport width under 1000px
+// //         if (currentMarginOffset < 0) {
+// //             currentMarginOffset += (scrollOffset + 50);
+// //             portfolioProjects.style.marginLeft = currentMarginOffset + "rem";
+// //         } else {
+// //             navigationLeft.style.opacity = ".3";
+// //         }
+// //     }
+// // }
+
 
 // function scrollLeft() {
-//     var intViewportWidth = window.innerWidth;
 //     navigationRight.style.opacity = "1";
 //     navigationLeft.style.opacity = "1";
 
-//     if (intViewportWidth >= 1000) { //scroll behavior for viewport width over 1000px
-//         if (currentMarginOffset < 0) {
-//             currentMarginOffset += scrollOffset;
-//             portfolioProjects.style.marginLeft = currentMarginOffset + "rem";
-//         } else {
-//             navigationLeft.style.opacity = ".3";
-//         }
-//     } else if (intViewportWidth < 1000) { //scroll behavior for viewport width under 1000px
-//         if (currentMarginOffset < 0) {
-//             currentMarginOffset += (scrollOffset + 50);
-//             portfolioProjects.style.marginLeft = currentMarginOffset + "rem";
-//         } else {
-//             navigationLeft.style.opacity = ".3";
-//         }
-//     }
+//     currentMarginOffset += getCardWidth();
+//     portfolioProjectCards.style.marginLeft = currentMarginOffset + "px";
 // }
-
-
-function scrollLeft() {
-    navigationRight.style.opacity = "1";
-    navigationLeft.style.opacity = "1";
-
-    currentMarginOffset += getCardWidth();
-    portfolioProjectCards.style.marginLeft = currentMarginOffset + "px";
-}
-
-function scrollRight() {
-    // var intViewportWidth = window.innerWidth;
-    navigationRight.style.opacity = "1";
-    navigationLeft.style.opacity = "1";
-
-    currentMarginOffset -= getCardWidth();
-    portfolioProjectCards.style.marginLeft = currentMarginOffset + "px";
-
-    
-    // if (intViewportWidth >= 1000) { //scroll behavior for viewport width over 1000px
-            
-    //     } else if (currentMarginOffset <= -30) {
-    //         navigationRight.style.opacity = ".3";
-    //     }
-}
 
 // function scrollRight() {
-//     var intViewportWidth = window.innerWidth;
-
+//     // var intViewportWidth = window.innerWidth;
 //     navigationRight.style.opacity = "1";
 //     navigationLeft.style.opacity = "1";
 
-//     if (intViewportWidth >= 1000) { //scroll behavior for viewport width over 1000px
-//         if (currentMarginOffset >= -30) {
-//             currentMarginOffset -= scrollOffset;
-//             portfolioProjects.style.marginLeft = currentMarginOffset + "rem";
-//         } else if (currentMarginOffset <= -30) {
-//             navigationRight.style.opacity = ".3";
-//         }
-//     } else if (intViewportWidth < 1000) { //scroll behavior for viewport width under 1000px
-//         if (currentMarginOffset >= -30) {
-//             currentMarginOffset -= (scrollOffset + 50);
-//             portfolioProjects.style.marginLeft = currentMarginOffset + "rem";
-//         } else if (currentMarginOffset <= -30) {
-//             navigationRight.style.opacity = ".3";
-//         }
-//     }
+//     currentMarginOffset -= getCardWidth();
+//     portfolioProjectCards.style.marginLeft = currentMarginOffset + "px";
+
+    
+//     // if (intViewportWidth >= 1000) { //scroll behavior for viewport width over 1000px
+            
+//     //     } else if (currentMarginOffset <= -30) {
+//     //         navigationRight.style.opacity = ".3";
+//     //     }
 // }
+
+// // function scrollRight() {
+// //     var intViewportWidth = window.innerWidth;
+
+// //     navigationRight.style.opacity = "1";
+// //     navigationLeft.style.opacity = "1";
+
+// //     if (intViewportWidth >= 1000) { //scroll behavior for viewport width over 1000px
+// //         if (currentMarginOffset >= -30) {
+// //             currentMarginOffset -= scrollOffset;
+// //             portfolioProjects.style.marginLeft = currentMarginOffset + "rem";
+// //         } else if (currentMarginOffset <= -30) {
+// //             navigationRight.style.opacity = ".3";
+// //         }
+// //     } else if (intViewportWidth < 1000) { //scroll behavior for viewport width under 1000px
+// //         if (currentMarginOffset >= -30) {
+// //             currentMarginOffset -= (scrollOffset + 50);
+// //             portfolioProjects.style.marginLeft = currentMarginOffset + "rem";
+// //         } else if (currentMarginOffset <= -30) {
+// //             navigationRight.style.opacity = ".3";
+// //         }
+// //     }
+// // }
 
 navigationLeft.addEventListener('click', scrollLeft);
 navigationRight.addEventListener('click', scrollRight);
