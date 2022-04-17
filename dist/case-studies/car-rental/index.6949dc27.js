145976f3fea7aeed18c155e3eb98d0ccdd9a98f0
marcin <38013623+voinar@users.noreply.github.com>
2022-04-17 23:12:43 +0200

    added 3 portfolio case studies

--- dist/case-studies/car-rental/index.6949dc27.js
+++
@@ -1,9 +0,0 @@
-// google tag manager script
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-    dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'G-0S6QR5S9PR');
-
-//# sourceMappingURL=index.6949dc27.js.map
---
+++ dist/index.3b91308c.js
@@ -0,0 +1,2 @@
+function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-0S6QR5S9PR");
+//# sourceMappingURL=index.3b91308c.js.map
