cd97c831c4ab6ee0d25e3125f8692fd372f7a822
marcin <38013623+voinar@users.noreply.github.com>
2022-04-25 22:39:31 +0200

    language versions: PL in progress

---
+++ src/pl/js/typewriter.js
@@ -0,0 +1,12 @@
+// ------------------------
+// HERO SECTION TYPEWRITER EFFECT
+// ------------------------
+
+import Typewriter from '/node_modules/typewriter-effect/dist/core';
+
+new Typewriter('#typewriter', {
+  strings: ['Odkryj na nowo', 'Zredefiniuj', 'Odmień'],
+  autoStart: true,
+  loop: true,
+  delay: 50,
+});
