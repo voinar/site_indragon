2a7864b83d9ccc8a906a6d3073f4d13132019d0d
marcin <38013623+voinar@users.noreply.github.com>
2022-05-04 22:14:10 +0200

    translation: PL
    translation & internal link structure adjustments (menu, footer)

--- src/pl/js/typewriter.js
+++ src/pl/js/typewriter.js
@@ -5,7 +5,7 @@
 import Typewriter from '/node_modules/typewriter-effect/dist/core';
 
 new Typewriter('#typewriter', {
-  strings: ['Odkryj na nowo', 'Zredefiniuj', 'Odmień'],
+  strings: ['Odśwież', 'Odkryj', 'Odmień'],
   autoStart: true,
   loop: true,
   delay: 50,
